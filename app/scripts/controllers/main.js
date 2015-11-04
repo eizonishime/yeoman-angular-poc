1bbd47dbbc8a2c4a8f3533164cac1ce9ce571157
Eizo Nishime <eizo.nishime@gmail.com>
2015-11-03 23:59:57 -0200

    Sample app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pocAngularYoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pocAngularYoApp
+ */
+angular.module('pocAngularYoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
